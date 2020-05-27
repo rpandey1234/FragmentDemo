bc7152eee284883ac600c187c187f7a6cd2276ae
Rahul Pandey <hello@rkpandey.com>
2020-05-27 15:29:05 -0700

    static fragment 1

---
+++ app/src/main/java/edu/stanford/rkpandey/fragmentdemo/FirstFragment.kt
@@ -0,0 +1,18 @@
+package edu.stanford.rkpandey.fragmentdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
