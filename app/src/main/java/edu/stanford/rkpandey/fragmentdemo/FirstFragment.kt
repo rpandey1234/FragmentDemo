1d1ae90c70a0573145b525e5377dd37ef24b6762
Rahul Pandey <hello@rkpandey.com>
2020-05-27 15:55:39 -0700

    use explicit onCreateView method for second fragment

--- app/src/main/java/edu/stanford/rkpandey/fragmentdemo/FirstFragment.kt
+++ app/src/main/java/edu/stanford/rkpandey/fragmentdemo/FirstFragment.kt
@@ -3,9 +3,6 @@ package edu.stanford.rkpandey.fragmentdemo
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 /**
  * A simple [Fragment] subclass.
--- app/src/main/java/edu/stanford/rkpandey/fragmentdemo/SecondFragment.kt
+++ app/src/main/java/edu/stanford/rkpandey/fragmentdemo/SecondFragment.kt
@@ -1,12 +1,21 @@
 package edu.stanford.rkpandey.fragmentdemo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 /**
  * A simple [Fragment] subclass.
  */
-class SecondFragment : Fragment(R.layout.fragment_second)
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+}
