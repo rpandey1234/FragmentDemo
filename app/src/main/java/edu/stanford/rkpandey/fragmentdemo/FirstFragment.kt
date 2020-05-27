3932c09dc129c980a4fb201261ac02f955452d38
Rahul Pandey <hello@rkpandey.com>
2020-05-27 15:46:42 -0700

    add button click listeners

--- app/src/main/java/edu/stanford/rkpandey/fragmentdemo/FirstFragment.kt
+++ app/src/main/java/edu/stanford/rkpandey/fragmentdemo/FirstFragment.kt
@@ -1,6 +1,7 @@
 package edu.stanford.rkpandey.fragmentdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +10,12 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
+private const val TAG = "FirstFragment"
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate FirstFragment")
     }
 
 }
--- app/src/main/java/edu/stanford/rkpandey/fragmentdemo/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/fragmentdemo/MainActivity.kt
@@ -1,12 +1,50 @@
 package edu.stanford.rkpandey.fragmentdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+        // Use a scope function!
+//        val transaction = supportFragmentManager.beginTransaction()
+//        transaction.replace(R.id.flFragment, firstFragment)
+//        transaction.commit()
+        // val adam = Person("Adam").apply {
+        //    age = 32
+        //    city = "London"
+        //}
+        //println(adam)
+//        val intent = Intent().apply {
+//            putExtra("key1", "value1")
+//            putExtra("key2", 2)
+//        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
